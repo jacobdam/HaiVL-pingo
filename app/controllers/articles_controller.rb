5640ecec1181eed7d91622a408af9e2389e53193
Phuc Dam <jacobdam@Phucs-MacBook-Pro.local>
2016-03-12 11:58:02 +0700

    Restyle comment section in article detail page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,8 +19,6 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
     @article.increase_views_count
-    @comment = Comment.new
-    @comments = @article.comments.order(created_at: :desc)
   end
 
   def edit
