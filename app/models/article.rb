2f73223e6a48a325e6df810dfeedcc72e903b292
Phuc Dam <jacobdam@Phucs-MacBook-Pro.local>
2016-03-13 10:35:31 +0700

    query hot articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,4 +14,18 @@ class Article < ActiveRecord::Base
   def increase_views_count
     Article.increment_counter(:views_count, self.id)
   end
+
+  def self.hot_articles
+    # SELECT articles.*, IFNULL(SUM(votes.value), 0)
+    # FROM articles
+    # LEFT JOIN votes
+    #   ON votes.article_id = articles.id
+    # GROUP BY articles.id
+    # ORDER BY SUM(votes.value) DESC
+    # LIMIT 5;
+    joins('LEFT JOIN votes ON votes.article_id = articles.id')
+      .group('articles.id')
+      .order('SUM(votes.value) DESC')
+      .limit(5)
+  end
 end
